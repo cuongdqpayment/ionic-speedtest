7a949d838d8009ccea1fba2bb0d66f5d39ae7dc8
cuongdq.payment <cuongdq.payment@gmail.com>
2019-02-18 07:33:31 +0700

    cuongdq push by sh

--- src/pages/speed-test/speed-test.ts
+++ src/pages/speed-test/speed-test.ts
@@ -23,7 +23,7 @@ var worker = null;
 //class dieu khien rieng cua no
 export class SpeedTestPage {
 
-  app:any = {
+  app: any = {
     id: "SPEEDTEST",
     name: "Speedtest VN",
     image: "assets/imgs/logo.png"
@@ -33,6 +33,12 @@ export class SpeedTestPage {
     graphName: 'Speed Test',
     unit: 'Mbps/ms',
   }
+
+  graphColor = {
+    statusColor: "#309030",
+    backgroundColor: "#E0E0E0",
+    progressColor: "#EEEEEE"
+  };
 
   objMeter = {};
 
@@ -45,7 +51,7 @@ export class SpeedTestPage {
   serverList = [];
 
   dynamicList: any = {
-     header: {
+    header: {
       title: "Time"
       , strong: "Server-ISP"
       , p: "Dowload"
@@ -56,12 +62,14 @@ export class SpeedTestPage {
   };
 
 
-  shareUrl:any;
+  shareUrl: any;
+
+  debug: any;
 
   constructor(
     private navCtrl: NavController,
     private apiLocation: ApiLocationService,
-    private apiGraph: ApiGraphService,
+    //private apiGraph: ApiGraphService,
     private modalCtrl: ModalController,
     private platform: Platform,
     private apiSpeedtest: ApiSpeedTestService,
@@ -69,66 +77,68 @@ export class SpeedTestPage {
     private authService: ApiAuthService,
     private apiSqlite: ApiSqliteService,
     private apiStorage: ApiStorageService
-    ) { }
+  ) { }
 
   ngOnInit() {
 
-    console.log('platform',this.platform.platforms());
-    console.log('cordova',this.platform.is("cordova"));
+    console.log('platform', this.platform.platforms());
+    console.log('cordova', this.platform.is("cordova"));
 
-    this.resetForm();  
+    this.resetForm();
 
     this.dynamicList.is_table = this.platform.platforms()[0] === 'core'
 
     this.apiSpeedtest.getSpeedtestServerList()
-    .then(list => {
-      this.serverList = list;
-      this.server = this.serverList[0];
-    })
-    .catch(err => {
-      console.log(err);
-    })
+      .then(list => {
+        this.serverList = list;
+        this.server = this.serverList[0];
+      })
+      .catch(err => {
+        console.log(err);
+      })
 
   }
-  
-  resetForm(){
-    if (this.platform.is("cordova")){
-      //this.results = this.apiSqlite.getResults();
-      let selectSql = {name:"SPEEDTEST"
-                          , cols:[
-                              { name:"time"}
-                              ,{ name:"result"}
-                          ]};
-      this.apiSqlite.selectAll(selectSql)
-          .then(data=>{
-            console.log('Select', data);
 
-          })
-          .catch(err=>{
-            console.log('err Select', err);
-          })
-    }else{
+  resetForm() {
+    if (this.platform.is("cordova")) {
+      //this.results = this.apiSqlite.getResults();
+      let selectSql = {
+        name: "SPEEDTEST"
+        , cols: [
+          { name: "time" }
+          , { name: "result" }
+        ]
+      };
+      this.apiSqlite.selectAll(selectSql)
+        .then(data => {
+          //console.log('Select all', JSON.stringify(data));
+          this.debug = 'Select all: ' + JSON.stringify(data);
+        })
+        .catch(err => {
+          console.log('err Select', err);
+        })
+    } else {
       this.results = this.apiStorage.getResults();
-    }    
-    
+    }
+
     //lay thong tin tu may chu
     //GET https://c3.mobifone.vn/api/ext-public/your-device?id=SPEEDTEST
     //return data.share_url for share post
     //duong dan gui ket qua {url:"https://c3.mobifone.vn/api/speedtest/post-result",method:"POST"}
     this.apiPublic.getMyDevice(this.app.id)
-    .then(data=>{
-      if (data) this.shareUrl = data.share_url; 
-    })
-    .catch(err=>{});
-    
+      .then(data => {
+        if (data) this.shareUrl = data.share_url;
+      })
+      .catch(err => { });
+
     this.resetContermet();
   }
 
   resetContermet() {
-    if (this.isRuning){
-      this.apiGraph.initUI();
+    if (this.isRuning) {
+      this.initUI();
       this.objMeter = this.objMeterOrigin;
-      this.apiGraph.updateUI({ state: 0, contermet: '...', progress: 0 });
+      this.updateUI({ state: 0, contermet: '...', progress: 0 });
     }
   }
 
@@ -137,31 +147,34 @@ export class SpeedTestPage {
     //this.hideShowTab();
 
     console.log('Finish Run!');
-    
-    if (this.results.length>0){
-      if (this.platform.is("cordova")){
-        if (this.result){
 
-          let insertSql = {name:"SPEEDTEST"
-          , cols:[
-            { name:"time", value: this.result.start_time}
-            ,{ name:"result", value: this.result}
-          ]};
+    if (this.results.length > 0) {
+      if (this.platform.is("cordova")) {
+        if (this.result) {
 
-          console.log('sql save',insertSql);
+          let insertSql = {
+            name: "SPEEDTEST"
+            , cols: [
+              { name: "time", value: this.result.start_time }
+              , { name: "result", value: this.result }
+            ]
+          };
+
+          //console.log('sql save',insertSql);
 
           this.apiSqlite.insert(insertSql)
-          .then(data=>{
+            .then(data => {
 
-            console.log('Insert', data);
+              console.log('Insert', JSON.stringify(data));
+              this.debug = 'Insert: ' + JSON.stringify(data);
 
-          })
-          .catch(err=>{
-            console.log('err Insert', err);
-          })
+            })
+            .catch(err => {
+              console.log('err Insert', err);
+            })
 
         }
-      }else{
+      } else {
         this.apiStorage.saveResults(this.results);
       }
     }
@@ -178,11 +191,11 @@ export class SpeedTestPage {
     this.isRuning = !this.isRuning;
 
     if (!this.isRuning) {
-      //this.apiGraph.I("startStopBtn").className = "";
+      //this.I("startStopBtn").className = "";
       //dung test
     } else {
       //lay vi tri de ghi ket qua
-      //this.apiGraph.I("startStopBtn").className = "running";
+      //this.I("startStopBtn").className = "running";
       //bat dau chay
       this.result = null;
       this.hideShowTab();
@@ -207,30 +220,30 @@ export class SpeedTestPage {
     let objCommand = e.data;
     //cap nhap nhan
     if (objCommand.command === 'init') {
-      this.initUI(objCommand.data);
+      this.initUIForm(objCommand.data);
     } else if (objCommand.command === 'progress') {
-      try{
-        this.apiGraph.updateUI({ state: 1, contermet: objCommand.data.contermet, progress: objCommand.data.progress });
-      }catch(e){}
+      try {
+        this.updateUI({ state: 1, contermet: objCommand.data.contermet, progress: objCommand.data.progress });
+      } catch (e) { }
     } else if (objCommand.command === 'finish') {
       this.updateResults(objCommand.work, objCommand.data);
     }
   }
 
-  initUI(formWork) {
+  initUIForm(formWork) {
     //gan ten cho thang do
     this.objMeter = {
       graphName: formWork.graphName,
       unit: formWork.unit,
     }
     //gan mau cho thang do
-    try{
-      this.apiGraph.initUI({
+    try {
+      this.initUI({
         statusColor: formWork.statusColor,
         backgroundColor: formWork.backgroundColor,
         progressColor: formWork.progressColor
       });
-    }catch(e){}
+    } catch (e) { }
   }
 
 
@@ -253,7 +266,7 @@ export class SpeedTestPage {
     //co cong viec va ket qua hoan thanh
     if (work == 'ip') {
       //cong viec hoan thanh lay ip
-      if (d.device&&d.device.ip_info) try{ d.device.ip_info = JSON.parse(d.device.ip_info);}catch(e){}
+      if (d.device && d.device.ip_info) try { d.device.ip_info = JSON.parse(d.device.ip_info); } catch (e) { }
       // console.log("device ***:", d.device);
       // console.log("server ***:", d.server);
       this.result.server = d.server;
@@ -318,7 +331,8 @@ export class SpeedTestPage {
               runNextTest();
             })
             .catch(err => {
-              console.log('err no get IP', err);
+              //console.log('err get IP', JSON.stringify(err));
+              runNextTest();
             });
         }
           break;
@@ -377,10 +391,9 @@ export class SpeedTestPage {
         default: nextIndex++;
       }
 
-      console.log('step',command);
+      //console.log('step',command);
       if (!command) {
-        console.log('stop');
-
+        //console.log('stop');
         this.clearRuning();
       }
 
@@ -401,14 +414,14 @@ export class SpeedTestPage {
           this.results.unshift(this.result);
         }
         //xem kq --send tu dong neu co url
-        console.log('result share location:',this.shareUrl);
+        console.log('result share location:', this.shareUrl);
         if (this.shareUrl) this.sendResult(this.shareUrl);
 
       })
       .catch(err => {
         //console.log(err);
         //ket qua send tu dong
-        console.log('result share no LOC:',this.shareUrl);
+        console.log('result share no LOC:', this.shareUrl);
         if (this.shareUrl) this.sendResult(this.shareUrl);
       });
 
@@ -419,21 +432,21 @@ export class SpeedTestPage {
    * @param shareUrl 
    * {url:"https://...", method:"POST", token:true|false}
    */
-  sendResult(shareUrl){
-    if (this.result&&shareUrl.url&&shareUrl.method) {
-      if (shareUrl.method=="POST"){
-        if (shareUrl.token){
+  sendResult(shareUrl) {
+    if (this.result && shareUrl.url && shareUrl.method) {
+      if (shareUrl.method == "POST") {
+        if (shareUrl.token) {
           this.authService.postDynamicForm(shareUrl.url, this.result)
             .then(data => {
-              console.log('save OK',data);
+              console.log('save OK', data);
             })
             .catch(err => {
               console.log('save err', err);
             });
-        }else{
+        } else {
           this.apiPublic.postDynamicForm(shareUrl.url, this.result)
             .then(data => {
-              console.log('save OK',data);
+              console.log('save OK', data);
             })
             .catch(err => {
               console.log('save err', err);
@@ -468,18 +481,18 @@ export class SpeedTestPage {
 
     let selectOptions = [];
 
-    this.serverList.forEach((el,idx)=>{
-      selectOptions.push({name: idx+1 + ". " + el.name, value: idx})
+    this.serverList.forEach((el, idx) => {
+      selectOptions.push({ name: idx + 1 + ". " + el.name, value: idx })
     })
 
     let data = {
       title: "Thiết lập"
       , items: [
-        { key: "server", name: "Lựa Chọn Máy chủ", hint:"Chọn ít nhất 1 cái nhé em", type: "select", value:0, options: selectOptions }
-        , { key:"is_table", name: "Kết quả kiểu bảng?", icon: "grid", value: this.dynamicList.is_table, type: "toggle" }
+        { key: "server", name: "Lựa Chọn Máy chủ", hint: "Chọn ít nhất 1 cái nhé em", type: "select", value: 0, options: selectOptions }
+        , { key: "is_table", name: "Kết quả kiểu bảng?", icon: "grid", value: this.dynamicList.is_table, type: "toggle" }
         , {
           type: "button", options: [
-            { name: "Chọn", next: "CALLBACK"}
+            { name: "Chọn", next: "CALLBACK" }
           ]
         }
       ]
@@ -495,15 +508,15 @@ export class SpeedTestPage {
     this.openModal(settings);
   }
 
-/**
- * Cau truc cua ham callback phai chuan nhu nay
- */
-  callbackSettings = function (that,res){
+  /**
+   * Cau truc cua ham callback phai chuan nhu nay
+   */
+  callbackSettings = function (that, res) {
     //console.log(res);
     if (res.data) this.dynamicList.is_table = res.data.is_table;
-    if (res.data&&res.data.server) this.server = this.serverList[res.data.server];
-    return new Promise((resolve, reject)=>{
-      resolve({next: "CLOSE"});
+    if (res.data && res.data.server) this.server = this.serverList[res.data.server];
+    return new Promise((resolve, reject) => {
+      resolve({ next: "CLOSE" });
     })
   }.bind(this);
 
@@ -513,21 +526,88 @@ export class SpeedTestPage {
     modal.present();
   }
 
-  viewResult(){
-    this.navCtrl.push(ResultsPage,{form:this.dynamicList,results:this.results,callback:this.callBack});
+  viewResult() {
+    this.navCtrl.push(ResultsPage, { form: this.dynamicList, results: this.results, callback: this.callBack });
   }
 
-  callBack = function(reset?:boolean){
+  callBack = function (reset?: boolean) {
     if (reset) this.resetForm();
   }.bind(this);
 
-  hideShowTab(){
+  hideShowTab() {
     /* let elements = document.querySelectorAll(".tabbar");
     if (elements != null) {
         Object.keys(elements).map((key) => {
             elements[key].style.display = (elements[key].style.display=='none'?"block":"none");
         });
     } */
+  }
+
+
+
+  ////////////////////////////////////////
+  // for UI
+  initUI(colors?: {
+    statusColor: string,
+    backgroundColor: string,
+    progressColor: string
+  }) {
+    if (colors) this.graphColor = colors; //doi mau default
+    this.drawMeter(this.I("dlMeter"), 0, 0);
+    this.I("dlText").textContent = "";
+  }
+
+  I(id) { return document.getElementById(id); }
+
+  drawMeter(c, amount, progress, colors?: {
+    statusColor: string,
+    backgroundColor: string,
+    progressColor: string
+  }) {
+
+    var myColors = (colors) ? colors : this.graphColor;
+
+    var ctx = c.getContext("2d");
+    var dp = window.devicePixelRatio || 1;
+    var cw = c.clientWidth * dp, ch = c.clientHeight * dp;
+    var sizScale = ch * 0.0055;
+    if (c.width == cw && c.height == ch) {
+      ctx.clearRect(0, 0, cw, ch);
+    } else {
+      c.width = cw;
+      c.height = ch;
+    }
+    ctx.beginPath();
+    ctx.strokeStyle = myColors.backgroundColor;
+    ctx.lineWidth = 16 * sizScale;
+    ctx.arc(c.width / 2, c.height - 58 * sizScale, c.height / 1.8 - ctx.lineWidth, -Math.PI * 1.1, Math.PI * 0.1);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.strokeStyle = myColors.statusColor;
+    ctx.lineWidth = 16 * sizScale;
+    ctx.arc(c.width / 2, c.height - 58 * sizScale, c.height / 1.8 - ctx.lineWidth, -Math.PI * 1.1, amount * Math.PI * 1.2 - Math.PI * 1.1);
+    ctx.stroke();
+    if (typeof progress !== "undefined") {
+      ctx.fillStyle = myColors.progressColor;
+      ctx.fillRect(c.width * 0.3, c.height - 16 * sizScale, c.width * 0.4 * progress, 4 * sizScale);
+    }
+  }
+
+  mbpsToAmount(s) {
+    return 1 - (1 / (Math.pow(1.3, Math.sqrt(s))));
+  }
+
+  msToAmount(s) {
+    return 1 - (1 / (Math.pow(1.08, Math.sqrt(s))));
+  }
+
+  updateUI(data: { state: 0 | 1, contermet: string, progress: number }) {
+    this.I("dlText").textContent = (data.state == 1 && data.contermet == '0') ? "..." : data.contermet;
+    this.drawMeter(this.I("dlMeter"), this.mbpsToAmount(Number(Number(data.contermet) * (data.state == 1 ? this.oscillate() : 1))), Number(data.progress));
+  }
+
+  oscillate() {
+    return 1 + 0.02 * Math.sin(Date.now() / 100);
   }
 
 }
