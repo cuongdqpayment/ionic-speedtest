f9059a95eb3a1bdad15451a2d15dbd456871a585
cuongdqpayment <cuongdq.payment@gmail.com>
2019-02-28 10:56:02 +0700

    cuongdq push by sh

--- deploy-android.sh
+++ deploy-android.sh
@@ -8,13 +8,14 @@ ionic cordova run android --device
 #ionic cordova build android --prod --release
 
 #2. get file: <app>\platforms\android\app\build\outputs\apk\release\app-release-unsigned.apk
+# copy to ./build to sign
 
-#3. use keytoll create key pair for sign:
-# keytool -genkey -v -keystore cng-release-key.keystore -alias alias_name -keyalg RSA -keysize 2048 -validity 10000
-# get file cng-release-key.keystore with pass when type
+#3. use keytool create key pair for sign:
+# keytool -genkey -v -keystore ./build/speedtest-app-key.keystore -alias speedtest-app-alias -keyalg RSA -keysize 2048 -validity 10000
+# get file ./build/cng-release-key.keystore with pass when type
 
 #4. sign apk file:
-# jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore cng-release-key.keystore speedtest-app-unsigned.apk speedtest-app
+# jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ./build/speedtest-app-key.keystore ./build/speedtest-app-unsigned.apk speedtest-app-alias
 
 #5. align file apk: 
 # zipalign -v 4 speedtest-app-unsigned.apk speedtest-app.apk
