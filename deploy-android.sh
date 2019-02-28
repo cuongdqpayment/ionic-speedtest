ce3e0e9dac00e6ff9a0cf6289121385d9497eb3c
cuongdqpayment <cuongdq.payment@gmail.com>
2019-02-28 13:58:43 +0700

    cuongdq push by sh

--- deploy-android.sh
+++ deploy-android.sh
@@ -4,15 +4,15 @@
 #ionic cordova build android --prod --release
 ionic cordova run android --device
 
-#1. build --> release
-#ionic cordova build android --prod --release
+#1. build --> release android 24.0 in window
+# ionic cordova build android --prod --release
 
-#2. get file: <app>\platforms\android\app\build\outputs\apk\release\app-release-unsigned.apk
-# copy to ./build to sign
+#2. -->get file: <app>\platforms\android\app\build\outputs\apk\release\app-release-unsigned.apk
+# -->copy to ./build to sign
 
 #3. use keytool create key pair for sign 4year=365*4+1:
 # keytool -genkey -v -keystore ./build/speedtest-app-key.keystore -alias speedtest-app-alias -keyalg RSA -keysize 2048 -validity 1461
-# get file ./build/cng-release-key.keystore with pass when type
+# -->get file ./build/cng-release-key.keystore with pass when type
 
 #4. sign apk file:
 # jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ./build/speedtest-app-key.keystore ./build/speedtest-app-unsigned.apk speedtest-app-alias
