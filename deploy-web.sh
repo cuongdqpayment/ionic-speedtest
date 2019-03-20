0147f0f1841d60f7be53ef2700ea7ef592925aeb
cuongdq.payment <cuongdq.payment@gmail.com>
2019-03-20 22:38:53 +0700

    cuongdq push by sh

--- deploy-web.sh
+++ deploy-web.sh
@@ -1,5 +1,8 @@
 
-#npm i http express fs url formidable path os mime-types node-html-parser cheerio
-#npm i sqlite3 bluebird dddos node-rsa jsonwebtoken
-ionic cordova build browser --prod --release
+#khai cac thanh phan trong package.json roi go lenh npm i la xong
+#npm i
+
+#buid dang browser de dung cordova.js
+ionic cordova build browser
+# --prod --release
 node server.js
